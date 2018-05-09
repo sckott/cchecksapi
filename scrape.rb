dc3a758b0d980f0de1de5bda0dd107bdfe26748b
Scott Chamberlain <myrmecocystus@gmail.com>
2018-05-08 17:07:46 -0700

    back to server mongo connections

--- scrape.rb
+++ scrape.rb
@@ -3,8 +3,8 @@ require "multi_json"
 require "oga"
 require "mongo"
 
-#$mongo = Mongo::Client.new([ ENV.fetch('MONGO_PORT_27017_TCP_ADDR') + ":" + ENV.fetch('MONGO_PORT_27017_TCP_PORT') ], :database => 'cchecksdb')
-$mongo = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'cchecksdb')
+$mongo = Mongo::Client.new([ ENV.fetch('MONGO_PORT_27017_TCP_ADDR') + ":" + ENV.fetch('MONGO_PORT_27017_TCP_PORT') ], :database => 'cchecksdb')
+# $mongo = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'cchecksdb')
 $cks = $mongo[:checks]
 
 def scrape_all
