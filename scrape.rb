9a9199f9802d98d68b2e76483c6fbee689e1986b
Scott Chamberlain <myrmecocystus@gmail.com>
2018-05-10 07:25:26 -0700

    back to server mongo setup

--- scrape.rb
+++ scrape.rb
@@ -6,8 +6,8 @@ require "oga"
 require "mongo"
 require "parallel"
 
-# $mongo = Mongo::Client.new([ ENV.fetch('MONGO_PORT_27017_TCP_ADDR') + ":" + ENV.fetch('MONGO_PORT_27017_TCP_PORT') ], :database => 'cchecksdb')
-$mongo = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'cchecksdb')
+$mongo = Mongo::Client.new([ ENV.fetch('MONGO_PORT_27017_TCP_ADDR') + ":" + ENV.fetch('MONGO_PORT_27017_TCP_PORT') ], :database => 'cchecksdb')
+# $mongo = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'cchecksdb')
 $cks = $mongo[:checks]
 
 def scrape_all
